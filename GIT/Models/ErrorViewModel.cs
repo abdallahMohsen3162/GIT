0185f4e483586af17687710cb9c7adadcbd45f4f
abdallahMohsen3162 <abdallah3162@gmail.com>
2024-08-29 11:37:42 +0300

    message

---
+++ GIT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GIT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
